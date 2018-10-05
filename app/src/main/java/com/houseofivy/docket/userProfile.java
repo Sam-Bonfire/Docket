fbba221f555a51c077828ad8e226efd90d4d6dc0
Pharry820 <mukulkadaskar@gmail.com>
2018-10-05 10:47:15 +0530

    .idea

---
+++ app/src/main/java/com/houseofivy/docket/userProfile.java
@@ -0,0 +1,31 @@
+package com.example.mukul.logindemo;
+
+public class userProfile {
+    public String userName;
+    public String userEmail;
+
+    public userProfile(){
+
+    }
+
+    public userProfile(String userName, String userEmail) {
+        this.userName = userName;
+        this.userEmail = userEmail;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+}

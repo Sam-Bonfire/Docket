8f918dad66c1dfbf8d584bc74de28504b5448b35
Sam-Bonfire <pprathamesh98@gmail.com>
2018-10-16 00:07:23 +0530

    Fixed R.java error

--- app/src/main/java/com/houseofivy/docket/timeTable.java
+++ app/src/main/java/com/houseofivy/docket/timeTable.java
@@ -8,7 +8,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-
+import com.houseofivy.docket.R;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
